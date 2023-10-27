65f986e1050e62e8f5391892b964345ed07be4d7
Golubev Alex <funtic1991@yandex.ru>
2023-10-27 22:44:26 +0300

    webpack connected

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
-const numbers = [2, 3, 5];
+// импорт
 
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
+import { initialCards } from './scripts/cards';
+import './pages/index.css';
 
-console.log(doubledNumbers); // 4, 6, 10
+// @todo: Темплейт карточки
+
+const cardTemplate = document.querySelector('#card-template').content;
+
+// @todo: DOM узлы
+
+const cardsPlace = document.querySelector('.places__list');
+
+// @todo: Функция создания карточки
+
+function createCard(cardData, delFunc){
+  
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = card.querySelector('.card__image');
+  const cardDeleteButton = card.querySelector('.card__delete-button');
+  const cardTitle = card.querySelector('.card__title');
+  
+  cardImage.src = cardData.link;
+  cardImage.alt = cardTitle.textContent = cardData.name;
+  
+  cardDeleteButton.addEventListener('click', delFunc);
+  
+  return card;
+
+}
+
+function renderCard(data, conteiner){
+    conteiner.append(createCard(data, handleDeleteCard));
+}
+
+// @todo: Функция удаления карточки
+
+function handleDeleteCard(evt) {
+
+  const deleteTarget = evt.target.closest('.card');
+  deleteTarget.remove();
+  
+}
+
+// @todo: Вывести карточки на страницу
+
+initialCards.forEach( (el) => {renderCard(el, cardsPlace)} )
